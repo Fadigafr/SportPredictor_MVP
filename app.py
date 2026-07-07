c350448bc6a655617f3c03e1bbb582c24fe87487
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-07-07 19:07:42 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+# SPORT PREDICTOR ULTRA PRO 2026
+import streamlit as st
+from database import init_db
+init_db()
+st.title("SPORT PREDICTOR ULTRA PRO 2026")
---
+++ database.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+def init_db():
+    conn=sqlite3.connect("users.db")
+    c=conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY,email TEXT,password TEXT)")
+    conn.commit()
+    conn.close()
