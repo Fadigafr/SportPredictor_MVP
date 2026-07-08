90f10dedba74cb1570c47c1e36583e9a516be16c
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-07-08 13:34:15 +0400

    Add hashing function and login page to auth module

--- auth.py
+++ auth.py
@@ -1,1 +1,12 @@
-# Authentication module
+import hashlib
+import streamlit as st
+
+def h(x):
+
+    return hashlib.sha256(
+        x.encode()
+    ).hexdigest()
+
+def login_page():
+
+    st.subheader("Connexion")
