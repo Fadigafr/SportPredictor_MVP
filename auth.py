b02509044516607285fb53c4346ec5cd2d89a26e
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-07-08 15:07:18 +0400

    Refactor login and registration functionality
    Refactor login function and enhance password handling with hashing.

--- auth.py
+++ auth.py
@@ -1,12 +1,76 @@
 import hashlib
 import streamlit as st
+from database import get_conn
 
-def h(x):
-
+def h(txt):
     return hashlib.sha256(
-        x.encode()
+        txt.encode()
     ).hexdigest()
 
-def login_page():
+def login():
 
-    st.subheader("Connexion")
+    conn = get_conn()
+    c = conn.cursor()
+
+    mode = st.sidebar.radio(
+        "Accès",
+        ["Connexion","Inscription"]
+    )
+
+    email = st.sidebar.text_input("Email")
+    password = st.sidebar.text_input(
+        "Mot de passe",
+        type="password"
+    )
+
+    if mode == "Inscription":
+
+        if st.sidebar.button("Créer compte"):
+
+            try:
+
+                c.execute(
+                    """
+                    INSERT INTO users(
+                        email,
+                        password
+                    )
+                    VALUES (?,?)
+                    """,
+                    (email,h(password))
+                )
+
+                conn.commit()
+
+                st.success(
+                    "Compte créé"
+                )
+
+            except:
+
+                st.error(
+                    "Email déjà utilisé"
+                )
+
+    if mode == "Connexion":
+
+        if st.sidebar.button("Connexion"):
+
+            user = c.execute(
+                """
+                SELECT *
+                FROM users
+                WHERE email=?
+                AND password=?
+                """,
+                (
+                    email,
+                    h(password)
+                )
+            ).fetchone()
+
+            if user:
+
+                st.session_state.user = email
+
+                st.rerun()
