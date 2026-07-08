3f12772bd681520533e19205af0c16bc287835de
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-07-08 13:34:51 +0400

    Update API key handling and add request headers
    Refactor API key retrieval and add request headers for API calls.

--- api_football.py
+++ api_football.py
@@ -1,2 +1,26 @@
 import streamlit as st
-API_KEY=st.secrets.get("API_KEY","")
+import requests
+
+API_KEY = st.secrets["API_KEY"]
+
+HEADERS = {
+    "x-apisports-key": API_KEY
+}
+
+def api_get(url):
+
+    try:
+
+        r = requests.get(
+            url,
+            headers=HEADERS,
+            timeout=20
+        )
+
+        return r.json()
+
+    except:
+
+        return {
+            "response": []
+        }
