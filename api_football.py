a56dce54148516c30d87e546d66419c8684f14b7
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-07-11 04:52:39 +0400

    Implement api_get function with error handling
    Add API key and function to make GET requests

--- api_football.py
+++ api_football.py
@@ -1,1 +1,25 @@
+import requests
 
+API_KEY = "5e4720fc37318c8dd95d856f2aaaebb1"
+
+def api_get(url):
+
+    try:
+
+        headers = {
+            "x-apisports-key": API_KEY
+        }
+
+        response = requests.get(
+            url,
+            headers=headers,
+            timeout=30
+        )
+
+        return response.json()
+
+    except Exception:
+
+        return {
+            "response": []
+        }
