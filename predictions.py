3b285a6497e979ef41b9886152176f47c1f5e0e8
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-07-17 15:33:59 +0000

    Refactor try-except block for top scorers retrieval

--- predictions.py
+++ predictions.py
@@ -211,22 +211,22 @@ def get_top_scorers(team_id, season):
     league_id = game["league"]["id"]
     season = game["league"]["season"]
 
-    try:
+        try:
 
-    home_scorers = get_top_scorers(
-        home_id,
-        season
-    )
+            home_scorers = get_top_scorers(
+            home_id,
+            season
+        )
 
-    away_scorers = get_top_scorers(
-        away_id,
-        season
-    )
+            away_scorers = get_top_scorers(
+            away_id,
+            season
+        )
 
-except:
+        except:
 
-    home_scorers = []
-    away_scorers = []
+        home_scorers = []
+        away_scorers = []
 
     probable_scorer = "Non disponible"
 
