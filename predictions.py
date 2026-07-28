a8aee98f2cabeefbde9fe02cbea9b4063cbffb0c
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-07-28 18:12:06 +0000

    Show top player in ATP Singles and Doubles
    Added display of the top player for both ATP Singles and Doubles.

--- predictions.py
+++ predictions.py
@@ -1642,6 +1642,7 @@ def tennis_page():
     
     st.subheader("🏆 ATP Singles Top 10")
 
+    st.write(singles_data[:1])
     for player in singles_data[:10]:
 
         st.write(
@@ -1652,6 +1653,7 @@ def tennis_page():
     
     st.subheader("🏆 ATP Doubles Top 10")
 
+    st.write(doubles_data[:1])
     for player in doubles_data[:10]:
 
         st.write(
