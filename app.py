b5990009174847eb66c4e439b896cf9a1105c788
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-07-30 13:27:23 +0000

    Add basketball calendar page functionality
    Added basketball_calendar_page import and updated sport handling.

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from database import init_db
 from api_basketball import get_games_today
 from predictions import dashboard_global_page
 from predictions import tennis_calendar_page
+from predictions import basketball_calendar_page
 
 init_db()
 
@@ -341,10 +342,7 @@ elif menu == "Calendrier":
         tennis_calendar_page()
 
     elif sport == "Basketball":
-
-        st.info(
-            "🏀 Calendrier Basketball en préparation"
-        )
+        basketball_calendar_page()
 
     elif sport == "Hockey":
 
