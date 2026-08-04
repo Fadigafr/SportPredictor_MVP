baa2188d03d320cfcd168d7b73c0a6afae78bc34
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-08-04 18:06:15 +0000

    Add predictions history JSON data

---
+++ results_db.py
@@ -0,0 +1,12 @@
+data/predictions_history.json
+[
+  {
+    "date": "2026-08-04",
+    "sport": "Football",
+    "match": "PSG vs Marseille",
+    "prediction": "PSG",
+    "ai_index": 92,
+    "odd": 1.85,
+    "result": "WIN"
+  }
+]
