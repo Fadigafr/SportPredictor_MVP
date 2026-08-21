d2ecc20b2014d81329f57a865252321a20c85156
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-08-21 12:54:45 +0000

    Uncomment database functions for predictions, users, and diagnostics

--- database.py
+++ database.py
@@ -99,7 +99,7 @@ def init_db():
     create_default_admin()
 
 
-#def count_predictions():
+def count_predictions():
 
     conn = get_conn()
     c = conn.cursor()
@@ -115,7 +115,7 @@ def init_db():
     return total
 
 
-#def count_users():
+def count_users():
 
     conn = get_conn()
     c = conn.cursor()
@@ -131,7 +131,7 @@ def init_db():
     return total
 
 
-# db_diagnostics():
+def db_diagnostics():
 
     conn = get_conn()
     c = conn.cursor()
