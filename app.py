9262a485161e276757c3ba47670d1629863939a8
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-08-23 17:01:15 +0000

    Remove test button for sports data retrieval
    Removed test button for live sports events.

--- app.py
+++ app.py
@@ -30,12 +30,6 @@ from api_bet365 import (
 
 from api_bet365 import get_live_events_sports
 
-if st.button("TEST SPORTS"):
-
-    data = get_live_events_sports()
-    st.write(type(data))
-    st.json(data)
-
 init_db()
 
 validate_football_results()
