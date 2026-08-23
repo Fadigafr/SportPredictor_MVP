08b5c30529a63e0fccd2be696e0a2d3fe33713dc
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-08-23 17:02:04 +0000

    Add button to test live sports events
    Added a button to test live sports events and display data.

--- test_bet365.py
+++ test_bet365.py
@@ -1,5 +1,11 @@
 from api_bet365 import get_soccer_live
 
+if st.button("TEST SPORTS"):
+
+    data = get_live_events_sports()
+    st.write(type(data))
+    st.json(data)
+    
 matches = get_soccer_live()
 
 for match in matches:
